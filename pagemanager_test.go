4b5f63f91096069f9aef3e6fedddf9210638cb68
Dom Aquino <dom.aquino@outlook.com>
2026-04-02 18:22:52 +0200

    Add test to check the size of the db file

--- pagemanager_test.go
+++ pagemanager_test.go
@@ -8,10 +8,21 @@ const (
 	DBFilename = "app.db"
 )
 
-func TestPageManager(t *testing.T) {
+func TestPageManagerCreator(t *testing.T) {
 	pm, err := PageManagerCreator(DBFilename)
 	if err != nil {
 		panic(err)
 	}
 	defer pm.file.Close()
+
+	fileInfo, err := pm.file.Stat()
+	if err != nil {
+		t.Fatalf("failed to stat db file: %v", err)
+	}
+	if fileInfo.IsDir() {
+		t.Fatalf("%s exists but is a directory, expected a file", DBFilename)
+	}
+	if fileInfo.Size() == 0 {
+		t.Fatalf("%s was created but is empty", DBFilename)
+	}
 }
